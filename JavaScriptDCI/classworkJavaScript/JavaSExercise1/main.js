d0e52e352f5ef1945d7589f6007025d87de32bcf
Ashenafi <asheaddis@yahoo.com>
2019-03-20 21:42:38 +0100

    JavaSExercise1/

---
+++ JavaScriptDCI/classworkJavaScript/JavaSExercise1/main.js
@@ -0,0 +1,47 @@
+/* console.log("Hello World!");
+
+console.log('This is my first JavaScript code.'); */
+
+
+/* var myName = ("Ashenafi");
+
+console.log(myName); */
+
+/* var profession = "student";
+
+console.log(profession);
+
+var salary = ("how much you earn per month");
+
+
+console.log("wow " + salary + " you are rich");
+
+var name ="are you Ashenafi?";
+confirm(name); */
+
+/* var movie = prompt ("What is your favorite movie?");
+prompt ("wowww  " + movie + " is my favorite movie too.");
+ 
+var food = prompt ("and what is your favorite food");
+prompt ("whatttt " + food + " is my favorite food too."); */
+
+ /* let myCity = "Addis-Ababa";
+console.log(myCity);
+
+let myCity = "New-York";
+console.log(myCity);  */
+
+
+/* var myCity = "Addis-Ababa";
+console.log(myCity);
+
+var myCity = "New-York";
+console.log(myCity); */
+
+                    /* to open a wab page */
+
+/* let want = confirm ("do you want to read the news?");
+
+if(want==true) {window.open("http://bbc.com");}
+else {window.open("https://google.com");} */
+
