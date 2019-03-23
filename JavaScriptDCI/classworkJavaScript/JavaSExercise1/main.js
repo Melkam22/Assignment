07043ff3d53b956bbc4b2d05f09a7f80515ad19d
Ashenafi <asheaddis@yahoo.com>
2019-03-23 17:14:03 +0100

    JavaSExercise1/

--- JavaScriptDCI/classworkJavaScript/JavaSExercise1/main.js
+++ JavaScriptDCI/classworkJavaScript/JavaSExercise1/main.js
@@ -1,47 +1,46 @@
-/* console.log("Hello World!");
-
-console.log('This is my first JavaScript code.'); */
 
 
-/* var myName = ("Ashenafi");
 
-console.log(myName); */
+console.log("Hello World!");
 
-/* var profession = "student";
+console.log('This is my first JavaScript code.'); 
+
+                               /* name  */
+let myName = ("Ashenafi");
+
+console.log(myName); 
+
+let profession = "student";
 
 console.log(profession);
 
-var salary = ("how much you earn per month");
+                                /* salary */
 
+let salary = ("how much do you earn per month?");
 
 console.log("wow " + salary + " you are rich");
 
-var name ="are you Ashenafi?";
-confirm(name); */
+let name ="are you Ashenafi?";
+console.log(name);
 
-/* var movie = prompt ("What is your favorite movie?");
+/* let movie = prompt ("What is your favorite movie?");
 prompt ("wowww  " + movie + " is my favorite movie too.");
  
-var food = prompt ("and what is your favorite food");
-prompt ("whatttt " + food + " is my favorite food too."); */
+let food = prompt ("and what is your favorite food");
+prompt ("whatttt " + food + " is my favorite food too.");
 
- /* let myCity = "Addis-Ababa";
+ let myCity = "Addis-Ababa";
 console.log(myCity);
 
 let myCity = "New-York";
-console.log(myCity);  */
+console.log(myCity); 
 
-
-/* var myCity = "Addis-Ababa";
-console.log(myCity);
-
-var myCity = "New-York";
-console.log(myCity); */
 
                     /* to open a wab page */
 
-/* let want = confirm ("do you want to read the news?");
+/*let want = confirm ("do you want to read the news?");
 
 if(want==true) {window.open("http://bbc.com");}
-else {window.open("https://google.com");} */
+else {window.open("https://google.com");} 
 
+ */
