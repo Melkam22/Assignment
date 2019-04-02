15026c1f2ace43a7254cb4358f0fb8cf23fd98a8
Ashenafi <asheaddis@yahoo.com>
2019-04-02 22:10:28 +0200

    ifFunction/

---
+++ JavaScriptDCI/classworkJavaScript/ThirdWeek/ifFunction/ifFunction.js
@@ -0,0 +1,60 @@
+
+let a = false;
+let b = "false";
+
+if (a != b ) {
+    console.log("Booleans are not equal to strings!");
+} else {
+    console.log("Booleans are equal to strings!");
+}
+
+                        /* second condition */
+
+let varOne = 10;
+let varTwo = 10;
+if(((varOne >= varTwo) && (120 % 2 === 0)) || (varOne * 260 ===2600)) {
+    console.log ("Heya!");
+(1 > 2) ? console.log("The condition is true") : console.log("false condition");
+
+    let varThree;
+    varThree = "1";
+
+    if (varThree = true) {
+        console.log("Heya is a string");
+    }
+}
+
+                            /* third condition */
+
+/* let salary1 = 1000;
+let salary2 =1500; */
+let salary3 = 3000;
+
+if (salary3 < 1100) {
+    console.log("you are poor.");
+} else if (salary3 == 1500) {
+    console.log("you are ok.");
+} else  (salary3 >=2800)
+{
+    console.log("you are rich");
+}
+
+
+                    /* fourth condition; time*/
+
+let time = 21;
+
+if (time < 10) {
+    greeting = "good morning!";
+} else if ((time >= 10) && (time < 20)) {
+    greeting = "good day!"
+} else {
+    greeting = "good evening!"
+}
+
+console.log(greeting);
+
+
+
+
+
